bc17da533a9567b2c79d260ae26472831e2eeb44
jackcdavey <jdavey@scu.edu>
2022-05-23 13:59:36 -0700

    removed unused state

--- src/App.tsx
+++ src/App.tsx
@@ -69,30 +69,21 @@ function runTest20() {
 
 function runCosineSimilarity(input: string[]) {
 	let res = input;
-	console.log(res);
+	
+
 	return res;
 }
 
 function runPearsonCorrelation(input: string[]) {
 	let res = input;
-	console.log(res);
+	
+
 	return res;
 }
 
 
 
 function App() {
-	let [test5Data, setTest5Data] = React.useState(test5)
-	let [test5Output, setTest5Output] = React.useState("")
-
-	let [test10Data, setTest10Data] = React.useState(test10)
-	let [test10Output, setTest10Output] = React.useState("")
-
-	let [test20Data, setTest20Data] = React.useState(test20)
-	let [test20Output, setTest20Output] = React.useState("")
-
-	let [trainData, setTrainData] = React.useState(train)
-
 	return <>
 		<button onClick={() => { runTest5() }}>Run Test 5</button>
 		<button onClick={() => { runTest10() }}>Run Test 10</button>
